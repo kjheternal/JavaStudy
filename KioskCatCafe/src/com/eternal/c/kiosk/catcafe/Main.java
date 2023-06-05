1aec7189bb2ab7a2835f7f4c48899c48b42b6a55
에테르넬 (J.H Kim) <132544056+kjheternal@users.noreply.github.com>
2023-06-05 11:33:26 +0900

    버전 11c 업로드

---
+++ KioskCatCafe/src/com/eternal/c/kiosk/catcafe/Main.java
@@ -0,0 +1,13 @@
+package com.eternal.c.kiosk.catcafe;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// 키오스크 클래스 객체 생성
+		Kiosk kiosk = new Kiosk();
+		
+		// 키오스크 작동
+		kiosk.Run();
+	}
+
+}
