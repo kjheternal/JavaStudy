f8cc58e8032a591d39358ec4ea3aa2fab01279b1
에테르넬 (J.H Kim) <132544056+kjheternal@users.noreply.github.com>
2023-06-09 15:01:31 +0900

    Delete javaboard_mysql directory

--- javaboard_mysql/JavaMysqlBoard/src/com/peisia/mysqltest/ProcMenuList.java
+++
@@ -1,9 +0,0 @@
-package com.peisia.mysqltest;
-
-import com.peisia.mysqlutil.Database;
-
-public class ProcMenuList {
-	static void run() {
-		Database.dbExecuteQuery("select * from myboard", true);
-	}
-}
--- javaboard_mysql/JavaMysqlBoard/src/module-info.java
+++
@@ -1,3 +0,0 @@
-module JavaMysqlBoard {
-	requires java.sql;
-}
